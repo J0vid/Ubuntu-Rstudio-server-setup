27afbc62f10098222e7d2a3c7f17a0bf14b59a5f
J. David Aponte <daponte08@gmail.com>
2022-04-26 12:54:25 -0600

    update for R > 4

--- library_setup.sh
+++ library_setup.sh
@@ -28,13 +28,14 @@ sudo apt-get install libcurl4-openssl-dev
 
 sudo apt-get install cifs-utils
 
-wget https://mran.blob.core.windows.net/install/mro/3.5.3/ubuntu/microsoft-r-open-3.5.3.tar.gz && tar -xf microsoft-r-open-3.5.3.tar.gz && cd microsoft-r-open/ && sudo ./install.sh
+wget https://cran.r-project.org/src/base/R-4/R-4.2.0.tar.gz && tar -xf R-4.2.0.tar.gz && cd R-4.2.0/ && sudo ./install.sh
 
-wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-1.3.776-amd64.deb && sudo gdebi rstudio-server-1.3.776-amd64.deb
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.1-461-amd64.deb
+sudo gdebi rstudio-server-2022.02.1-461-amd64.deb
 
 sudo rstudio-server start
 
-sudo mount.cifs //10.41.72.40/Storage /mnt -o user=jose.aponte,vers=3.0,dir_mode=0777,file_mode=0777
+#sudo mount.cifs //10.41.72.40/Storage /mnt -o user=jose.aponte,vers=3.0,dir_mode=0777,file_mode=0777
 
 # a few useful ubuntu installs to run Bioconductor (eg DOQTL) packages
 sudo apt-get install libxml2-dev
@@ -42,5 +43,3 @@ sudo apt-get install r-cran-rmysql
 sudo apt-get install libmariadb-client-lgpl-dev
 
 ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'
-
-
